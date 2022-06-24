0fe0de41204e39561c10c87ec944eece08127240
Gabriel Antonio de Lima <105071638+Gabriel07201@users.noreply.github.com>
2022-06-23 22:32:12 -0300

    first_try_branch

--- teste_exemplo/first_test.py
+++ teste_exemplo/first_test.py
@@ -2,3 +2,4 @@ print('here I create a new paste and one archive.py')
 print('So I want to see how the Git will identify it.')
 print('In my firsts tries, I used the GitHub Desktop.')
 print("Here I don't use any branch, so this is not the best possibility.")
+print("Here I create a new branch to see how branch's works ") # test branch "teste1"
