4c11835ce6e7d76de1a2df9bb2077ae906d1c295
Gabriel Antonio de Lima <105071638+Gabriel07201@users.noreply.github.com>
2022-06-23 22:45:57 -0300

    Update first_test.py

--- teste_exemplo/first_test.py
+++ teste_exemplo/first_test.py
@@ -4,3 +4,4 @@ print('In my firsts tries, I used the GitHub Desktop.')
 print("Here I don't use any branch, so this is not the best possibility.")
 print('This is the branch, I created to learn how to merge two branches.') # branch test "teste2"
 print("Here I create a new branch to see how branches works ") # test branch "teste1"
+print("I liked that the branches have an error and even so it was possible to solve and merge the two changes.")
