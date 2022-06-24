e07744dc33bf03a87884129a13f52e6b4d0edf2a
Gabriel Antonio de Lima <105071638+Gabriel07201@users.noreply.github.com>
2022-06-23 22:36:53 -0300

    test_branch2

--- teste_exemplo/first_test.py
+++ teste_exemplo/first_test.py
@@ -2,3 +2,4 @@ print('here I create a new paste and one archive.py')
 print('So I want to see how the Git will identify it.')
 print('In my firsts tries, I used the GitHub Desktop.')
 print("Here I don't use any branch, so this is not the best possibility.")
+print('This is the branch, I created to learn how to merge two branches.') # branch test "teste2"
